d62145f7a05e64c2b37932afabff33eaa01ba587
salla kontio <sallakatariinakontio@gmail.com>
2022-09-20 10:33:28 +0300

    Created API methods get, get by ID, create and post for recipes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,40 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
+const {
+  readRecipe,
+  readRecipes,
+  createRecipe,
+  deleteRecipes,
+} = require("./database");
+
+app.get("/recipe", async (req, res) => {
+  console.log("Reading recipies");
+  return res.json(await readRecipes());
+});
+
+app.get("/recipe/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(`Reading recipe at ${id}`);
+  res.json(await readRecipe(id));
+});
+
+app.post("/recipe", async (req, res) => {
+  console.log("Creating recipe");
+  await createRecipe(req.body);
+  res.sendStatus(200);
+});
+
+app.delete("/recipe/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(`Deleted person at id point ${id}`);
+  await deleteRecipes(id);
+  res.sendStatus(200);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
